82eaafd1c2dbe26c4e99d642de4abe76072fbdc1
Burek-man <Alnu07001@utb.vaxjo.se>
2025-01-10 13:12:58 +0100

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    // Binärsökning på en lista av personer, söker efter personnummer
+    static Person BinarySearch(List<Person> personer, int personnr)
+    {
+        int left = 0;
+        int right = personer.Count - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            // Hämta person från listan
+            Person midPerson = personer[mid];
+
+            // Jämför personnummer
+            if (midPerson.Personnr == personnr)
+            {
+                return midPerson; // Hittade personen
+            }
+            else if (midPerson.Personnr < personnr)
+            {
+                left = mid + 1; // Sök i högra delen
+            }
+            else
+            {
+                right = mid - 1; // Sök i vänstra delen
+            }
+        }
+
+        return null; // Om personen inte finns i listan
+    }
+
+    static void Main()
+    {
+        // Skapa en lista med personer
+        List<Person> personer = new List<Person>
+        {
+            new Person(1001, "Alice"),
+            new Person(1002, "Bob"),
+            new Person(1003, "Charlie"),
+            new Person(1004, "David"),
+            new Person(1005, "Eve")
+        };
+
+        // Exempel på att söka efter en person
+        int söktPersonnr = 1003;
+        Person person = BinarySearch(personer, söktPersonnr);
+
+        if (person != null)
+        {
+            Console.WriteLine($"Hittade {person.Namn} med personnr {person.Personnr}");
+        }
+        else
+        {
+            Console.WriteLine($"Person med personnr {söktPersonnr} finns inte.");
+        }
+        
+    }
+}
+if
--- obj/Debug/net8.0/tim-yppgift-sokningar.AssemblyInfo.cs
+++ obj/Debug/net8.0/tim-yppgift-sokningar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tim-yppgift-sokningar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5e2f695e8be61e32de29fb0e912ca34ccba19d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b618ef64c9916d04f4b679aa09547c9ec01865f")]
 [assembly: System.Reflection.AssemblyProductAttribute("tim-yppgift-sokningar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tim-yppgift-sokningar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
